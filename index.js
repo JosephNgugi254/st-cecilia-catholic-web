0780caa0138237f0689b0990ea89a1fed8978ea0
josephNgugi <solitaryjoe069@gmail.com>
2024-06-07 17:12:26 +0300

    ade some changes

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ class SpecialHeader extends HTMLElement {
                     <li><a class="dropdown-item" href="./brief-history.html">Brief History</a></li>
                     <li><a class="dropdown-item" href="./church-leardership.html">Church leadership</a></li>
                     <li><a class="dropdown-item" href="./contact-us.html">Contact Us</a></li>
+                    <li><a class="dropdown-item" href="./out-stations.html">Out Stations</a></li>
                 </ul>
               </li>          
     
@@ -166,6 +167,12 @@ class SpecialFooter extends HTMLElement {
                       </ul>
                   </div>            
               </div>
+
+              <div id="scrollTopBtn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="black" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+              </svg>
+              </div>
           
               <div class="d-flex flex-column flex-sm-row justify-content-between border-top footer-copyright">
                   <p>copyright © 2024 St Cecilia Catholic parish, Isinya. All rights reserved.</p>
@@ -182,3 +189,28 @@ class SpecialFooter extends HTMLElement {
 customElements.define('special-header', SpecialHeader);
 customElements.define('special-footer', SpecialFooter);
 // end of header and footer management queries
+
+
+
+
+
+
+
+// scroll to top button element
+// Get the button element
+var scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// Show the button when scrolling down
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+// Scroll to  top when the button is clicked
+scrollTopBtn.onclick = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+};
